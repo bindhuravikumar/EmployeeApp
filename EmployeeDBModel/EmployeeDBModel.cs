fb725571345fded2fdac303b067293ef0d88ee80
bindhuravikumar <47471485+bindhuravikumar@users.noreply.github.com>
2019-04-09 18:01:34 +0530

    Add files via upload

---
+++ EmployeeDBModel/EmployeeDBModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDBModel
+{
+    public class EmployeeContext : DbContext
+    {
+
+        static string setting = ConfigurationManager.ConnectionStrings["employeeeDBConnection"].ConnectionString;
+
+
+        public EmployeeContext() : base(setting)
+        {
+            Database.SetInitializer(new CreateDatabaseIfNotExists<EmployeeContext>());
+        }
+
+        public DbSet<Employee> Employee { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
